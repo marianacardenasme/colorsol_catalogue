2815bc35172476737e227ad1630a28633f99b4ed
Mariana Cárdenas <111990215+marianacardenasme@users.noreply.github.com>
2023-01-01 15:46:48 -0500

    update

---
+++ build/precache-manifest.d0c709b82c07fe198090388d33d24de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "449fc7c59b10d65d81b7",
+    "url": "/static/css/main.82cd3e0b.chunk.css"
+  },
+  {
+    "revision": "449fc7c59b10d65d81b7",
+    "url": "/static/js/main.449fc7c5.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "4e6f6d3c366933ccfb60",
+    "url": "/static/js/2.4e6f6d3c.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "367b0296f445daa7868a3fab630d9f72",
+    "url": "/index.html"
+  }
+];
